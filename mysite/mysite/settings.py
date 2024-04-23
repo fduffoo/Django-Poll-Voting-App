e41354263c425ead774bf079fb49c614fb252e5b
FernandoDuffoo <fduffoo@aol.com>
2024-04-22 21:27:27 -0400

    Project set up, part 7 complete.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,12 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = "mysite.urls"
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
